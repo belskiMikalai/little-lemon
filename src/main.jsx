ca899dd716579f0d2ef4206301848cccf494bd05
Belski Mikalai <belskimikalai@gmail.com>
2025-03-26 19:31:09 +0300

    switch to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { HashRouter, Routes, Route } from 'react-router';
 import './index.css';
 import App from './App.jsx';
 import Main from './components/main_structure/Main.jsx';
@@ -10,14 +10,14 @@ import Reservation from './components/reservation/Reservation.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={ <App /> }>
           <Route path='/' element={ <Main/> } />
           <Route path='/reservation/*' element={ <Reservation /> } />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
 
