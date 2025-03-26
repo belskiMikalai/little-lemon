970b6209300da42b72f49139bf2da85f021b75b1
Belski Mikalai <belskimikalai@gmail.com>
2025-03-26 19:58:52 +0300

    change basename

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import Reservation from './components/reservation/Reservation.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <HashRouter basename='/little-lemon'>
       <Routes>
         <Route element={ <App /> }>
           <Route path='/' element={ <Main/> } />
